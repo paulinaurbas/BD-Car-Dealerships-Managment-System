8972983b99a753ab80bcb98ce645653e54c21fc3
Michał Herjan <michal@herjan.pl>
2020-03-14 15:13:33 +0100

    Part 1/5
    Yeet 1

---
+++ BD2_CAR_DEAL/BD2_CAR_DEAL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BD2_CAR_DEAL.MvcApplication" Language="C#" %>
---
+++ BD2_CAR_DEAL/BD2_CAR_DEAL/Models/IndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BD2_CAR_DEAL.Models
+{
+    public class IndexModel
+    {
+        public string Test {get; set;}
+    }
+}
